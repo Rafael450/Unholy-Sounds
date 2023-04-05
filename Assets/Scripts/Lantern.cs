3c91156d1b1b4b0260ef3ea1014fb557726088e4
Rafael450 <rafaelgamer450@gmail.com>
2023-04-05 03:00:03 -0300

    feat: now lantern is directional

---
+++ Assets/Scripts/Lantern.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lantern : MonoBehaviour
+{
+    void Start()
+    {
+        // Hide the mouse pointer when the game starts
+        
+    }
+    
+    void Update()
+    {
+        Cursor.visible = false;
+        // Get the position of the mouse pointer in screen coordinates
+        Vector3 mousePos = Input.mousePosition;
+
+        // Calculate the direction from the light position to the mouse position
+        Vector3 direction = Camera.main.ScreenToWorldPoint(mousePos) - transform.position;
+        // direction.z = 0; // Set the Z component to zero to keep the rotation in 2D
+
+        // Rotate the light to face the mouse direction
+        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(-angle, Vector3.forward);
+    }
+}
