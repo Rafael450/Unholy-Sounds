0436b50118bd0bd716436e6a9d7906a202100798
moisesmgft <moisesmgft@gmail.com>
2023-04-25 18:52:59 -0300

    lantern class

---
+++ Assets/Scripts/Classes/Character.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    // [SerializeField] private string characterName;
+    // [SerializeField] private float moveSpeed;
+
+    public string characterName;
+
+    public float moveSpeed;
+
+    public Lantern lantern;
+
+
+    // ideias: escudo, boostSpeed, lucky
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
