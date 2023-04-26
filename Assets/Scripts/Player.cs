aac23367f000214fd61cc419e1af955f3ae09316
moisesmgft <moisesmgft@gmail.com>
2023-04-26 07:13:47 -0300

    picking items

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
+
 
 
 public class Player : MonoBehaviour
@@ -15,11 +17,17 @@ public class Player : MonoBehaviour
     public int luck = 5;
     public bool active = true;
     public Flashlight flashlight;
+    public CollectibleItem collect;
     private UnityEngine.Rendering.Universal.Light2D light2d;
     private Movimento movement;
+    public GameObject letterE;
+    private int velvaCounter = 0;
 
 
-    // ideias: escudo, boostSpeed, lucky
+    public float timer = 0;
+    private EnergyDrinkType currentPowerUp;
+    public bool isPowerUpActive = false;
+
 
     void Start()
     {
@@ -36,6 +44,84 @@ public class Player : MonoBehaviour
         light2d = GameObject.FindGameObjectWithTag("PlayerLight").GetComponent<Light2D>();;
         light2d.pointLightOuterAngle = Flashlight.getOuterAngle(flashlightType);
         light2d.pointLightOuterRadius = Flashlight.getOuterRadius(flashlightType);
+    
+        letterE = GameObject.FindGameObjectWithTag("LetterE");
     }
 
+    void Update() {
+        if (timer <= 0 && isPowerUpActive == false) return;
+        if (timer <= 0 && isPowerUpActive) {
+            movement.speed -= EnergyDrink.getSpeedBoost(currentPowerUp);
+            isPowerUpActive = false;
+            return;
+        }
+        timer -= Time.deltaTime;
+    }
+
+    void powerUp(GameObject gameObject) {
+        if (isPowerUpActive) {
+            movement.speed -= EnergyDrink.getSpeedBoost(currentPowerUp);
+            isPowerUpActive = false;
+        }
+        currentPowerUp = gameObject.GetComponent<EnergyDrink>().itemType;
+        timer = EnergyDrink.getDuration(currentPowerUp);
+        movement.speed += EnergyDrink.getSpeedBoost(currentPowerUp);
+        isPowerUpActive = true;
+
+    }
+
+    void newVelva() {
+        velvaCounter++;
+        if (velvaCounter == 5) 
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    void newFlashlight(GameObject gameObject) {
+        flashlightType = gameObject.GetComponent<Flashlight>().flashlightType;
+        light2d.pointLightOuterAngle = Flashlight.getOuterAngle(flashlightType);
+        light2d.pointLightOuterRadius = Flashlight.getOuterRadius(flashlightType);
+    }
+
+    void Pickup(Collider2D other)
+    {
+        // Delete the item from the scene
+        switch (other.gameObject.tag)
+        {
+            case "Energy":
+                // Do something if the object's tag is "Energy"
+                powerUp(other.gameObject);
+
+                break;
+            case "Velva":
+                // Do something if the object's tag is "Velva"
+                newVelva();
+                break;
+            case "Flashlight":
+                // Do something if the object's tag is "Flashlight"
+                newFlashlight(other.gameObject);
+                break;
+            default:
+                break;
+        }
+
+        Destroy(other.gameObject);
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        letterE.SetActive(true);
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        letterE.SetActive(false);
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(Input.GetKey(KeyCode.E))
+        {
+            Pickup(other);
+        }
+    }
 }
