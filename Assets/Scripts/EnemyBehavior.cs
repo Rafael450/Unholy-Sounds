05827fcf25d2aaaa6d69709e373be09cc331225d
Rafael450 <rafaelgamer450@gmail.com>
2023-04-26 02:35:24 -0300

    created sound system

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -8,18 +8,35 @@ public class EnemyBehavior : MonoBehaviour
     [SerializeField] Transform target;
     NavMeshAgent nm;
     public GameObject Enemy;
+
+    private SoundController sound;
     void Start()
     {
+        sound = Enemy.GetComponent<SoundController>();
+
         nm = GetComponent<NavMeshAgent>();
         nm.updateRotation = false;
         nm.updateUpAxis = false;
         target = GameObject.Find("Player").transform;
+
+        Debug.Log(sound);
+
+        sound.PlayStartSound();
     }
 
     void Update()
     {
         nm.SetDestination(target.position);
-        transform.position = Enemy.GetComponent<Transform>().position - new Vector3(0,0.7f,0);
+        transform.position = Enemy.GetComponent<Transform>().position;
+        if((target.position - Enemy.GetComponent<Transform>().position).magnitude >= 100) // distance
+        {
+            float angle = Random.Range(0f, Mathf.PI * 2);
+            Vector3 randomVector = (new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0))*25;
+            
+            Enemy.GetComponent<Transform>().position = target.position + randomVector;
+            sound.PlayAproxSound();
+        }
+        
     }
     
 }
---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    private AudioSource audioSource; // Drag and drop an AudioSource component onto this variable in the Inspector
+    public AudioClip aproxSound; 
+    public AudioClip startSound; 
+    public AudioClip angerSound;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Call this function to play the audio clip
+    public void PlayAproxSound()
+    {
+        audioSource.PlayOneShot(aproxSound); // Play the audio clip once using the audio source
+    }
+
+    public void PlayStartSound()
+    {
+        audioSource.PlayOneShot(startSound); // Play the audio clip once using the audio source
+    }
+
+    public void PlayAngerSound()
+    {
+        audioSource.PlayOneShot(angerSound); // Play the audio clip once using the audio source
+    }
+}
