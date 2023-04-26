34b49066717cfa8760641271f930c193d0683ba8
moisesmgft <moisesmgft@gmail.com>
2023-04-26 01:48:39 -0300

    PLAYER PREFAB DONE

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -2,34 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flashlight : Item
-{
-    public enum FlashlightType {
+
+public enum FlashlightType {
         Type1,
         Type2,
         Type3,
-        Type4
-    }
+        Type4,
+        Type5
+}
 
-    public FlashlightType itemType;
+public class Flashlight : Item
+{
 
-    private float getOuterAngle() {
-        switch (itemType) {
+
+    public FlashlightType flashlightType;
+
+    public float getOuterAngle() {
+        switch (flashlightType) {
         default:
         case FlashlightType.Type1: return 90;
         case FlashlightType.Type2: return 50;
         case FlashlightType.Type3: return 150;
         case FlashlightType.Type4: return 120;
+        case FlashlightType.Type5: return 360;
         }
     }
 
-    private float getOuterRadius() {
-        switch (itemType) {
+    public float getOuterRadius() {
+        switch (flashlightType) {
         default:
         case FlashlightType.Type1: return 20;
         case FlashlightType.Type2: return 25;
         case FlashlightType.Type3: return 10;
         case FlashlightType.Type4: return 17;
+        case FlashlightType.Type5: return 20;
         }
     }
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
 
 public class Player : MonoBehaviour
 {
@@ -8,24 +10,27 @@ public class Player : MonoBehaviour
     // [SerializeField] private float moveSpeed;
 
     public string characterName;
-
     public float moveSpeed;
-
-    public Flashlight flashlight;
-
-    public UnityEngine.Rendering.Universal.Light2D light;
+    public FlashlightType flashlightType;
+    private Flashlight flashlight;
+    private UnityEngine.Rendering.Universal.Light2D light2d;
+    private Movimento movement;
 
 
     // ideias: escudo, boostSpeed, lucky
 
     void Start()
     {
-        
-        
+        movement = gameObject.AddComponent<Movimento>();
+        movement.speed = moveSpeed;
+
+        flashlight = gameObject.AddComponent<Flashlight>();
+        flashlight.flashlightType = flashlightType;
+
+        light2d = GameObject.FindGameObjectWithTag("PlayerLight").GetComponent<Light2D>();;
+        light2d.pointLightInnerAngle = 37;
+        light2d.pointLightOuterAngle = flashlight.getOuterAngle();
+        light2d.pointLightOuterRadius = flashlight.getOuterRadius();
     }
 
-    void Update()
-    {
-        
-    }
 }
